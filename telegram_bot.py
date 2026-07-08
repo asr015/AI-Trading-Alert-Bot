a4acd050fbeab2ea7f788f4a5493b7b0675124f4
asr015 <satnamsingh021987@gmail.com>
2026-07-08 20:10:12 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,14 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
 
+def send_message(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )

df148d82b2d505755a6df71deab8c58f411135ed
asr015 <satnamsingh021987@gmail.com>
2026-07-08 21:26:35 +0530

    Create watchlist.py

---
+++ watchlist.py
@@ -0,0 +1,20 @@
+WATCHLIST = [
+
+    "BEL.NS",
+    "HAL.NS",
+    "RVNL.NS",
+    "IRFC.NS",
+    "BDL.NS",
+    "COCHINSHIP.NS",
+    "MAZDOCK.NS",
+    "BHEL.NS",
+    "NTPC.NS",
+    "NHPC.NS",
+
+    "RELIANCE.NS",
+    "SBIN.NS",
+    "ICICIBANK.NS",
+    "HDFCBANK.NS",
+    "TATAMOTORS.NS",
+
+]

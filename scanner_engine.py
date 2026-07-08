5fdf45cd74304d0ee5cd2cd0289503f9161abaf7
asr015 <satnamsingh021987@gmail.com>
2026-07-08 21:28:25 +0530

    Update scanner_engine.py

--- scanner_engine.py
+++ scanner_engine.py
@@ -1,8 +1,17 @@
+from indicator_engine import calculate_indicators
+
 def calculate_score(data):
+
+    df = calculate_indicators(data)
 
     score = 0
 
-    if len(data) > 0:
-        score += 20
+    last = df.iloc[-1]
+
+    if last["EMA20"] > last["EMA50"]:
+        score += 30
+
+    if last["RSI"] > 60:
+        score += 30
 
     return score

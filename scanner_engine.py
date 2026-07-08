90603a1ef0559a80c5fd32da38e295c850da5144
asr015 <satnamsingh021987@gmail.com>
2026-07-08 22:44:33 +0530

    Update scanner_engine.py

--- scanner_engine.py
+++ scanner_engine.py
@@ -1,4 +1,6 @@
 from indicator_engine import calculate_indicators
+from smart_money_engine import smart_money_score
+
 
 def calculate_score(data):
 
@@ -8,47 +10,67 @@ def calculate_score(data):
             "reasons": ["No Data"]
         }
 
+
     df = calculate_indicators(data)
 
     last = df.iloc[-1]
     prev = df.iloc[-2]
 
-    score = 0
+
+    technical_score = 0
     reasons = []
+
 
     # EMA Trend
     if last["EMA20"] > last["EMA50"]:
-        score += 30
+        technical_score += 30
         reasons.append("✅ EMA Bullish")
 
-    # RSI
+
+    # RSI Strength
     if last["RSI"] > 60:
-        score += 30
+        technical_score += 30
         reasons.append("✅ RSI Strong")
+
 
     # Price Above EMA20
     if last["Close"] > last["EMA20"]:
-        score += 20
+        technical_score += 20
         reasons.append("✅ Above EMA20")
+
 
     # Bullish Candle
     if last["Close"] > last["Open"]:
-        score += 20
+        technical_score += 20
         reasons.append("✅ Bullish Candle")
+
 
     # EMA Rising
     if last["EMA20"] > prev["EMA20"]:
-        score += 30
+        technical_score += 30
         reasons.append("✅ EMA Rising")
+
 
     # Volume
     avg_volume = df["Volume"].tail(20).mean()
 
     if last["Volume"] > avg_volume:
-        score += 40
+        technical_score += 40
         reasons.append("✅ Volume Expansion")
 
+
+    # Smart Money Check
+    smart_money = smart_money_score(data)
+
+    final_score = technical_score + smart_money["score"]
+
+
+    reasons.extend(
+        smart_money["reasons"]
+    )
+
+
     return {
-        "score": score,
+        "score": final_score,
         "reasons": reasons
     }

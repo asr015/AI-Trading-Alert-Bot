1c4caf30ca0f2da6449b62d102dde7e6af0e654e
asr015 <satnamsingh021987@gmail.com>
2026-07-08 21:23:16 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,6 +4,9 @@ def get_scanner_data(symbol):
 
     stock = yf.Ticker(symbol)
 
-    data = stock.history(period="5d", interval="15m")
+    data = stock.history(
+        period="5d",
+        interval="15m"
+    )
 
     return data

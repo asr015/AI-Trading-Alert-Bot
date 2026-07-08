4bcfb5050338106b686cdabc825f89c6a3c38c83
asr015 <satnamsingh021987@gmail.com>
2026-07-08 20:34:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from nifty import get_nifty_data
+from market_status import get_market_status
 from telegram_bot import send_message
 
 data = get_nifty_data()
+market_status = get_market_status()
 
 message = f"""
+━━━━━━━━━━━━━━━━━━
 📊 TradingASR AI
 
 📈 NIFTY 50 : {data['price']}
 📉 Change : {data['change']} ({data['change_pct']}%)
 
-{data['status']}
+{market_status}
 
 🕒 {data['time']}
 
 🤖 Bot Status : Active ✅
+━━━━━━━━━━━━━━━━━━
 """
 
 send_message(message)

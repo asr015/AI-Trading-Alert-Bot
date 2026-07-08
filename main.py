197486ae4e8a0ba94c16145086fd44fbd0765ef2
asr015 <satnamsingh021987@gmail.com>
2026-07-08 17:05:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+import os
+from telegram import Bot
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+def send_message():
+    bot = Bot(token=BOT_TOKEN)
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text="Bot Working ✅\n\nGitHub → Telegram connection successful."
+    )
+
+if __name__ == "__main__":
+    send_message()

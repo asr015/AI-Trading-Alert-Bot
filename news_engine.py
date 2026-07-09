5a10210987d137c29445d35d4a1a4d8285f0e38c
asr015 <satnamsingh021987@gmail.com>
2026-07-09 20:49:55 +0530

    Add news_score function for news scoring

---
+++ news_engine.py
@@ -0,0 +1,15 @@
+import requests
+
+def news_score(symbol):
+
+    score = 0
+    reasons = []
+
+    try:
+        # Future: News API integration
+        # response = requests.get(...)
+
+        return score, reasons
+
+    except Exception:
+        return 0, []

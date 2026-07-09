3e054b62f8780decde4c9838915c216649ed4983
asr015 <satnamsingh021987@gmail.com>
2026-07-09 20:25:23 +0530

    Refactor calculate_risk to calculate_trade with DataFrame
    Refactored the risk calculation function to calculate trade parameters based on a DataFrame and score. The function now returns trade details for both buying and selling scenarios.

--- risk_engine.py
+++ risk_engine.py
@@ -1,22 +1,43 @@
-def calculate_risk(score):
+def calculate_trade(df, score):
 
-    if score >= 220:
+    last = df.iloc[-1]
 
-        return {
-            "risk": "🟢 LOW",
-            "rr": "1:3"
-        }
+    atr = last["ATR"]
 
-    elif score >= 160:
+    # BUY
+    if score >= 0:
 
-        return {
-            "risk": "🟡 MEDIUM",
-            "rr": "1:2"
-        }
+        entry = round(last["High"] + atr * 0.20, 2)
 
+        sl = round(last["Low"] - atr * 0.50, 2)
+
+        risk = entry - sl
+
+        target1 = round(entry + risk * 2, 2)
+
+        target2 = round(entry + risk * 3, 2)
+
+        trade = "BUY"
+
+    # SELL
     else:
 
-        return {
-            "risk": "🔴 HIGH",
-            "rr": "Avoid Trade"
-        }
+        entry = round(last["Low"] - atr * 0.20, 2)
+
+        sl = round(last["High"] + atr * 0.50, 2)
+
+        risk = sl - entry
+
+        target1 = round(entry - risk * 2, 2)
+
+        target2 = round(entry - risk * 3, 2)
+
+        trade = "SELL"
+
+    return {
+        "trade": trade,
+        "entry": entry,
+        "sl": sl,
+        "target1": target1,
+        "target2": target2
+    }

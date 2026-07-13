9031a7d820013122051ee841c7c8800b757971f5
asr015 <satnamsingh021987@gmail.com>
2026-07-13 12:22:59 +0530

    Improve error handling in get_stock_data function
    Refactor get_stock_data function to handle exceptions and return empty DataFrame on errors.

--- scanner.py
+++ scanner.py
@@ -1,15 +1,33 @@
+# ==========================================
+# TradingASR AI Pro v2.0
+# File : scanner.py
+# ==========================================
+
+import pandas as pd
 import yfinance as yf
+
 from config import TIMEFRAME, PERIOD
 
 
 def get_stock_data(symbol):
 
-    stock = yf.Ticker(symbol)
+    try:
 
-    data = stock.history(
-        period=PERIOD,
-        interval=TIMEFRAME,
-        auto_adjust=True
-    )
+        stock = yf.Ticker(symbol)
 
-    return data
+        data = stock.history(
+            period=PERIOD,
+            interval=TIMEFRAME,
+            auto_adjust=True
+        )
+
+        if data is None or data.empty:
+            return pd.DataFrame()
+
+        return data
+
+    except Exception as e:
+
+        print(f"{symbol}: {e}")
+
+        return pd.DataFrame()

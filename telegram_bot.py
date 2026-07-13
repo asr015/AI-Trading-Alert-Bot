32be2215b37da4cd5baa33ffb135d41c519173b6
asr015 <satnamsingh021987@gmail.com>
2026-07-13 12:25:59 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,34 @@
+# ==========================================
+# TradingASR AI Pro v2.0
+# File : telegram_bot.py
+# ==========================================
+
 import requests
+
 from config import BOT_TOKEN, CHAT_ID
+
 
 def send_message(message):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+    try:
+
+        response = requests.post(
+            url,
+            data={
+                "chat_id": CHAT_ID,
+                "text": message
+            },
+            timeout=15
+        )
+
+        response.raise_for_status()
+
+        return True
+
+    except requests.RequestException as e:
+
+        print(f"Telegram Error: {e}")
+
+        return False
